1697b60dfb857fa5703b8cf5dbc098c6aae0d7ed
jdToralla <vos2madre@gmail.com>
2021-09-17 17:30:31 -0600

    Primeros 3 reportes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { dataD } from './data/data.constants';
 import { ApiService } from './services/api.service';
 
 @Component({
@@ -10,70 +11,109 @@ export class AppComponent {
 
   title = 'Smart Chair';
   dataHistory = []
-  data
+  labelsThend = []
+  dataThend = []
+  typeGraph1: string
+  typeGraph2: string
+  data1 = {}
+  data2 = {}
+  data = []
+  contDias = [0, 0, 0, 0, 0, 0, 0]
+
 
   constructor(private apiService: ApiService) { }
 
   ngOnInit() {
-    this.apiService.getAllData().subscribe(res => {
-      res.map((val: any) => {                
+    // this.apiService.getAllData().subscribe(res => {
+
+    // console.log(res);
+    let res = dataD
+    this.data = res
+    this.getDataThendGraph(res)
+    this.getDataHistory(res)
+    this.getDataBarGraph(res)
+
+    // })
+  }
+
+  getDataHistory(data: object[]): void {
+    data.map((val: any) => {
+      if (val.tipo == 1) {
         this.dataHistory.push({
           date: val.fecha_inicio.split('T')[0],
           startTime: val.fecha_inicio.split('T')[1].split('.')[0],
           endTime: val.fecha_fin.split('T')[1].split('.')[0]
         })
-      })      
-      console.log('Data', this.dataHistory);
+      }
     })
   }
 
-  
+  getDataThendGraph(data: object[]): void {
 
-  // 1. Thend graph
-  type = 'line';
-  data1 = {
-    labels: ["01/09/2021", "02/09/2021", "03/09/2021", "04/09/2021", "05/09/2021", "06/09/2021", "07/09/2021"],
-    datasets: [
-      {
-        label: "Peso en kg",
-        data: [65, 66, 67, 65, 69, 64, 71],
-        fill: false,
-        borderColor: '#dd3b56',
-        tension: 0.1
+    data.map((val: any) => {
+      if (val.tipo == 2) {
+        this.labelsThend.push(val.fecha_fin.split('T')[0])
+        this.dataThend.push(val.valor)
       }
-    ]
-  };
+    })
+    this.labelsThend.reverse()
+    this.dataThend.reverse()
+
+    // 1. Thend graph
+    this.typeGraph1 = 'line';
+    this.data1 = {
+      labels: this.labelsThend,
+      datasets: [
+        {
+          label: "Peso en lb",
+          data: this.dataThend,
+          fill: false,
+          borderColor: '#dd3b56',
+          tension: 0.1
+        }
+      ]
+    };
+  }
 
 
-  // 2. Bar graph
-  type2 = 'bar';
-  labelsBar = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo']
-  data2 = {
-    labels: this.labelsBar,
-    datasets: [{
-      label: 'Cantidad',
-      data: [6, 5, 8, 8, 5, 7, 5, 4],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-        'rgba(255, 205, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(201, 203, 207, 0.2)'
-      ],
-      borderColor: [
-        'rgb(255, 99, 132)',
-        'rgb(255, 159, 64)',
-        'rgb(255, 205, 86)',
-        'rgb(75, 192, 192)',
-        'rgb(54, 162, 235)',
-        'rgb(153, 102, 255)',
-        'rgb(201, 203, 207)'
-      ],
-      borderWidth: 1
-    }]
-  };
+  getDataBarGraph(data: object[]): void {
+    data.map((val: any) => {
+      if (val.tipo == 1) {
+        let numeroDia = new Date(val.fecha_fin.split('T')[0] + " " + val.fecha_fin.split('T')[1].split('.')[0]).getDay();
+        this.contDias[numeroDia]++
+      }
+    })
+
+    // 2. Bar graph
+    this.typeGraph2 = 'bar';
+    this.data2 = {
+      labels: ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'],
+      datasets: [{
+        label: 'Cantidad',
+        data: this.contDias,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 205, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(201, 203, 207, 0.2)'
+        ],
+        borderColor: [
+          'rgb(255, 99, 132)',
+          'rgb(255, 159, 64)',
+          'rgb(255, 205, 86)',
+          'rgb(75, 192, 192)',
+          'rgb(54, 162, 235)',
+          'rgb(153, 102, 255)',
+          'rgb(201, 203, 207)'
+        ],
+        borderWidth: 1
+      }]
+    }
+
+  }
 
   //General options for graphics
   options = {
@@ -81,15 +121,10 @@ export class AppComponent {
     // maintainAspectRatio: false
   };
 
-
-
   test($event: any) {
     console.log('Test', $event.value);
-
+    let test = this.data.filter(r=> r.fecha_fin.split('T')[1].split('.')[0] == $event.value)
   }
-
-
-
 }
 
 
